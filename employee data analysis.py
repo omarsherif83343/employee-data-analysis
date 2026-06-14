1ae28514ae679892461cfca82e29c5a40a659012
Omar Sherif <134719521+omarsherif83343@users.noreply.github.com>
2026-06-14 15:44:27 +0300

    Add files via upload

---
+++ employee data analysis.py
@@ -0,0 +1,60 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+r_csv = pd.read_csv("C:\\employees_data.csv")
+#Cleaning Data
+
+r_csv.dropna(inplace=True)
+r_csv.drop_duplicates(inplace=True)
+
+#Analysis
+
+total_salary = r_csv["Salary"].sum()
+print(total_salary)
+
+
+avg_salary = r_csv["Salary"].mean()
+print(avg_salary)
+
+
+max_salary = r_csv["Salary"].max()
+print(max_salary)
+
+min_salary = r_csv["Salary"].min()
+print(min_salary)
+
+count_employees = r_csv.groupby("Department")["Employee_ID"].count()
+
+average_salary_department = r_csv.groupby("Department")["Salary"].mean()
+print(average_salary_department)
+print(average_salary_department.idxmax())
+
+city_count = r_csv.groupby("City")["Employee_ID"].count()
+print(city_count.idxmax())
+
+#Charts
+
+plt.figure(figsize=(10,6))
+count_employees.plot(kind='bar',label="Employee Count")
+plt.title("Employee Count")
+plt.title("Number of Employee by Department")
+plt.xlabel("Department")
+plt.xticks(rotation = 0)
+plt.ylabel("Number of Employee")
+plt.show()
+
+plt.figure(figsize=(10,6))
+average_salary_department.plot(kind='bar',label = "Average Salary")
+plt.title("Average Salary by Department")
+plt.xlabel("Department")
+plt.xticks(rotation = 0)
+plt.ylabel("Average Salary")
+plt.show()
+
+
+city_count.plot(kind='pie',autopct='%1.1f%%')
+plt.title("Employee Distribution by City")
+plt.legend(title="Cities",loc="best")
+plt.show()
+
